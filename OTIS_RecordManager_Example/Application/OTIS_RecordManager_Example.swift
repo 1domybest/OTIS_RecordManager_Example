197ce6445562cbd8e0b7835f2c27145992054ab7
OTIS <72382663+1domybest@users.noreply.github.com>
2024-11-05 15:32:48 +0900

    add Main Class
    add Main Class

---
+++ OTIS_RecordManager_Example/Application/OTIS_RecordManager_Example.swift
@@ -0,0 +1,17 @@
+//
+//  OTIS_CameraManager_ExampleApp.swift
+//  OTIS_CameraManager_Example
+//
+//  Created by 온석태 on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct OTIS_RecordManager_Example: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
